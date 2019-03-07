ee201bfb7359f6f101420156002e39e3eeda176a
AanandBhandari <aanandbhandari143@gmail.com>
2019-03-07 17:43:11 +0545

    made bricks works

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const config = {
         update: update
     }
 };
-let platforms;
+let brick;
 let paddle;
 let cursors;
 let ball;
@@ -23,7 +23,7 @@ let game = new Phaser.Game(config);
  function preload() {
      this.load.image('paddle', 'assets/PNG/Webp.net-resizeimage.png');
      this.load.image('ball', 'assets/PNG/Webp.net-resizeimage (1).png');
-     this.load.image('ground', 'assets/PNG/17-Breakout-Tiles.png');
+     this.load.image('brick', 'assets/PNG/Webp.net-resizeimage (2).png');
  }
  function create() {
      this.physics.world.setBoundsCollision(true, true, true, false);
@@ -31,15 +31,37 @@ let game = new Phaser.Game(config);
      ball.setData('onPaddle', true);
      paddle = this.physics.add.image(400, 550, 'paddle').setCollideWorldBounds(true).setImmovable();
      this.physics.add.collider(ball, paddle, hitPaddle, null, this);
+     bricks0 = this.physics.add.staticGroup({
+         key: 'brick',
+         repeat: 7,
+         setXY: { x: 150, y: 150, stepX: 78 }
+     });
+     bricks1 = this.physics.add.staticGroup({
+         key: 'brick',
+         repeat: 4,
+         setXY: { x: 250, y: 200, stepX: 98 }
+     });
+     bricks2 = this.physics.add.staticGroup({
+         key: 'brick',
+         repeat: 6,
+         setXY: { x: 50, y: 250, stepX: 118 }
+     });
+     
+     this.physics.add.collider(ball, bricks0, hitBrick, null, this);
+     this.physics.add.collider(ball, bricks1, hitBrick, null, this)
+     this.physics.add.collider(ball, bricks2, hitBrick, null, this)
 
     //  inputs
      cursors = this.input.keyboard.createCursorKeys();
+     
  }
  function update() {
      if (cursors.left.isDown) {
-         paddle.setVelocityX(-160);
+        //  paddle.setVelocityX(-160);
+        paddle.x -=3;
      } else if (cursors.right.isDown) {
-         paddle.setVelocityX(160);
+        //  paddle.setVelocityX(160);
+        paddle.x +=3;
      } 
      if (cursors.up.isDown) {
          if (ball.getData('onPaddle')) {
@@ -47,23 +69,32 @@ let game = new Phaser.Game(config);
              ball.setData('onPaddle', false);
          }
      }
+     if (ball.y > 600) {
+         resetBall();
+     }
  }
  function hitPaddle(ball,paddle) {
      let diff = 0;
 
      if (ball.x < paddle.x) {
-         //  Ball is on the left-hand side of the paddle
          diff = paddle.x - ball.x;
          ball.setVelocityX(-10 * diff);
      }
      else if (ball.x > paddle.x) {
-         //  Ball is on the right-hand side of the paddle
          diff = ball.x - paddle.x;
          ball.setVelocityX(10 * diff);
      }
      else {
-         //  Ball is perfectly in the middle
-         //  Add a little random X to stop it bouncing straight up!
          ball.setVelocityX(2 + Math.random() * 8);
      }
  }
+
+ function hitBrick(ball,bricks) {
+     bricks.disableBody(true, true);
+ }
+
+function resetBall () {
+    ball.setVelocity(0);
+    ball.setPosition(paddle.x, 500);
+    ball.setData('onPaddle', true);
+}
